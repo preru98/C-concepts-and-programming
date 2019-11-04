54fcd07de5fc4a82a157cf38a22ffee517bdcc13
Prerna <prerna1998.ps@gmail.com>
2019-11-04 23:27:32 +0530

    Delegate Illustration

---
+++ DelegateIllustration/DelegateIllustration/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateIllustration
+{
+    public delegate void myDelegate();
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Illustration I = new Illustration();
+            myDelegate Obj = new myDelegate(I.func);
+            Obj();
+            Console.Read();
+        }
+    }
+    class Illustration
+    {
+        public void func()
+        {
+            Console.WriteLine("I am from func method");
+        }
+    }
+}
