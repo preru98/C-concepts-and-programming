19232f8cc012beace789b4ded16e338a26e3a1e2
Prerna <prerna1998.ps@gmail.com>
2019-11-04 23:30:51 +0530

    Initialising using anonymous method

---
+++ anonymousDelegate/anonymousDelegate/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace anonymousDelegate
+{
+    public delegate void myDelegate(int n);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+    
+            temp = Int32.Parse(Console.ReadLine());
+            myDelegate M = delegate (int val)
+            {
+                Console.WriteLine("Lets Start Counting");
+                for (int i = 1; i <= val; i++)
+                {
+                    Console.WriteLine(i);
+                }
+            };
+            M(temp);
+            Console.Read();
+        }
+    }
+
+}
