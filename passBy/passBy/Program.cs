f397b20b2a2c060f5288cade3e606df4f688f21c
Prerna <prerna1998.ps@gmail.com>
2019-11-10 23:44:40 +0530

    Pass by ref/val

---
+++ passBy/passBy/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace passBy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=5;
+            int b=90;
+            Program P = new Program();
+            
+            Console.WriteLine("Before Swapping");
+            Console.WriteLine("a:" + a);
+            Console.WriteLine("b:" + b);
+            Console.WriteLine("");
+
+            P.swap(a, b);
+            Console.WriteLine("After Swapping through pass by value");
+            Console.WriteLine("a:" + a);
+            Console.WriteLine("b:" + b);
+            Console.WriteLine("");
+
+
+            P.swapRef(ref a, ref b);
+            Console.WriteLine("After Swapping through pass by reference");
+            Console.WriteLine("a:" + a);
+            Console.WriteLine("b:" + b);
+            Console.WriteLine("");
+
+            Console.Read();
+        }
+        public void swap(int x, int y)
+        {
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
+     
+        }
+        public void swapRef(ref int x, ref int y)
+        {
+            int temp;
+            temp = x;
+            x = y;
+            y = temp;
+            
+        }
+    }
+   
+}
