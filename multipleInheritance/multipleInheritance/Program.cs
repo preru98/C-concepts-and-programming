fa46127941e0d705019716affed730d8befda8fb
Prerna <prerna1998.ps@gmail.com>
2019-11-10 23:47:09 +0530

    Multiple Inheritance

---
+++ multipleInheritance/multipleInheritance/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace multipleInheritance
+{  
+    interface interfaceA
+    {
+        void school(); 
+    }
+
+    interface interfaceB
+    {
+        void college();
+    }
+
+    class baseClassA : interfaceA
+    {
+        public void school()
+        {
+            Console.WriteLine("School Name : Yuvshakti Model School");
+        }
+    }
+
+    class baseClassB : interfaceB
+    {
+        public void college()
+        {
+            Console.WriteLine("College Name : Jagan Institute of Management Studies");
+        }
+    }
+
+    class Program :interfaceA, interfaceB
+    {
+        baseClassA objA = new baseClassA();
+        baseClassB objB = new baseClassB();
+        public void school()
+        {
+            objA.school();
+        }
+        public void college()
+        {
+            objB.college();
+        }
+        static void Main(string[] args)
+        {
+            Program P = new Program();
+            P.school();
+            P.college();
+            Console.ReadKey();
+        }
+    }
+}
