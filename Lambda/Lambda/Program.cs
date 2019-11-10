a925b0fc926b0c789c22ada1b6094ccac94027ce
Prerna <prerna1998.ps@gmail.com>
2019-11-10 23:43:11 +0530

    Lambda

---
+++ Lambda/Lambda/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    public delegate string myDelegate(int n);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp;
+
+            temp = Int32.Parse(Console.ReadLine());
+            myDelegate M = (val) => 
+            {
+                Console.WriteLine("Lets Start Counting");
+                for (int i = 1; i <= val; i++)
+                {
+                    Console.WriteLine(i);
+                }
+                return "ABC";
+            };
+            M(temp);
+            Console.Read();
+        }
+    }
+}
