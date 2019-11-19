282e8fde6ee09225a98e1cf0c7627a1aa4c9eb3c
Prerna <prerna1998.ps@gmail.com>
2019-11-19 23:28:05 +0530

    Event Handling in CUI

---
+++ EventOne/EventOne/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventOne
+{ public delegate void eventHandler();            //declaration of delegate
+
+    class myControl
+    {
+        public event eventHandler someEvent;
+        public void onsomeEvent()
+        {
+            if (someEvent != null)
+                someEvent();
+        }
+    }
+    class Program
+    {   static void handle()
+        {
+            Console.WriteLine("Hello");
+        }
+        static void Main(string[] args)
+        {
+            myControl M = new myControl();
+            M.someEvent += handle;
+            M.onsomeEvent();
+            Console.Read();
+        }
+    }
+}
