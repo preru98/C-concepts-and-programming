82a27c201d61158f553d78b57a24949dcaf73e2d
Prerna <prerna1998.ps@gmail.com>
2019-11-19 23:24:54 +0530

    LINQ 1

---
+++ LINQ _P/LINQ _P/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_P
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 20, 56, -9, 67, 32, 67 };
+            var queryArray = from num in array
+                             orderby num descending
+                             select num;
+            Console.WriteLine(" Descending order : ");
+            foreach (int i in queryArray)
+                Console.Write(i + " | ");
+            Console.Read();
+
+        }
+    }
+}
