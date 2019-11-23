575320fb3436298d5b05206afe0221cb8bf43bc5
Prerna <prerna1998.ps@gmail.com>
2019-11-23 23:55:32 +0530

    Action Delegate

---
+++ ActionDelegate/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionDelegate
+{
+    class Program
+    {   static void square()
+        {
+            Console.WriteLine("x * x");
+        }
+        public static void Main(string[] args)
+        {
+            Action myDelegate = square;
+            myDelegate();
+            Console.ReadKey();
+            //Action -> Min 1 & Max 16 input characters and 0 output parameters
+        }
+    }
+}
