40b49301dcedea749a84abfb2e05906499cacf72
Prerna <prerna1998.ps@gmail.com>
2020-01-04 23:20:58 +0530

    Object Array

---
+++ objectArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace objectArray
+{   class ArrayObjects
+    {
+        int count = 10;
+        String name;
+
+        public ArrayObjects()
+        {
+            Console.Write("Object Created");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayObjects[] A = new ArrayObjects[10];
+            for (int i = 0; i < 10; i++)
+            {
+                 A[i] = new ArrayObjects(); Console.WriteLine(i+1);
+            }
+            Console.Read();
+        }
+
+    }
+}
